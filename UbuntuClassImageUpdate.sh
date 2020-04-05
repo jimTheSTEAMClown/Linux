6847e3c17290a05e3dea62c6637c9e04fa5f16d8
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-04 18:16:34 -0700

    Update UbuntuClassImageUpdate.sh

--- UbuntuClassImageUpdate.sh
+++ UbuntuClassImageUpdate.sh
@@ -47,6 +47,7 @@ done
 # echo "update the software install to update with main universe restricted multiverse"
 # echo "----------------------------------------------------"
 # sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+$ grep ^[^#] /etc/apt/sources.list /etc/apt/sources.list.d/*
 
 echo "Do you wish to run $ sudo apt-get update -y?"
 select yn in "Yes" "No"; do
