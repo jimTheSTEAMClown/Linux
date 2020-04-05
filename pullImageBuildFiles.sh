fcca3216c7fd6a51d5571bfa4cd10bf624d11ea7
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 09:37:57 -0700

    Create pullImageBuildFiles.sh

---
+++ pullImageBuildFiles.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo wget https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate.sh
+sudo chmod 755 UbuntuClassImageUpdate.sh
+sudo cat UbuntuClassImageUpdate.sh
+# sudo bash UbuntuClassImageUpdate.sh
