acd3591202db37bde8205b138085f7fd23b3ffb6
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 14:04:22 -0700

    Update UbuntuClassImageUpdate-Chrome.sh

--- UbuntuClassImageUpdate-Chrome.sh
+++ UbuntuClassImageUpdate-Chrome.sh
@@ -5,10 +5,13 @@ echo "----------------------------------------------------"
 echo "installing Chrome"
 echo "----------------------------------------------------"
 echo " "
+echo "----------------------------------------------------"
 echo "sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+echo "----------------------------------------------------"
 echo "sudo apt install ./google-chrome-stable_current_amd64.deb -y"
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
+echo "----------------------------------------------------"
 echo "cat /etc/apt/sources.list.d/google-chrome.list"
 cat /etc/apt/sources.list.d/google-chrome.list
 echo " "
