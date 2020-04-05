3fd1c41c8c4252a1aed8d2dc9771db9439c4f233
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 13:20:18 -0700

    Update pullImageBuildFiles.sh

--- pullImageBuildFiles.sh
+++ pullImageBuildFiles.sh
@@ -10,11 +10,30 @@ echo " ----------------------------------------------------"
 sudo wget -O ./classImageBuild/UbuntuClassImageUpdate.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate.sh
 sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate.sh
 sudo cat ./classImageBuild/UbuntuClassImageUpdate.sh
+
 echo " ----------------------------------------------------"
 echo "VLC build file"
 echo " ----------------------------------------------------"
 sudo wget -O ./classImageBuild/UbuntuClassImageUpdate-VLC.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate-VLC.sh
 sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate-VLC.sh
+
+echo " ----------------------------------------------------"
+echo "Tixati build file"
+echo " ----------------------------------------------------"
+sudo wget -O ./classImageBuild/UbuntuClassImageUpdate-Tixati.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate-Tixati.sh
+sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate-Tixati.sh
+
+echo " ----------------------------------------------------"
+echo "Visual Studio Code build file"
+echo " ----------------------------------------------------"
+sudo wget -O ./classImageBuild/UbuntuClassImageUpdate-VSC.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate-VSC.sh
+sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate-VSC.sh
+
+echo " ----------------------------------------------------"
+echo "Chrome build file"
+echo " ----------------------------------------------------"
+sudo wget -O ./classImageBuild/UbuntuClassImageUpdate-Chrome.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate-Chrome.sh
+sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate-Chrome.sh
 
 echo " ----------------------------------------------------"
 echo "Done copying all the classImageBuild files"
