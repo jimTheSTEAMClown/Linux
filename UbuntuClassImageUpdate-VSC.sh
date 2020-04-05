9792c5a8ec1a1ede3243c8dc5191adb6abe247ea
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 14:03:23 -0700

    Update UbuntuClassImageUpdate-VSC.sh

--- UbuntuClassImageUpdate-VSC.sh
+++ UbuntuClassImageUpdate-VSC.sh
@@ -7,13 +7,27 @@ echo "----------------------------------------------------"
 echo "Installing Visual Studion Code, which is a Coding IDE"
 echo "----------------------------------------------------"
 echo " "
+echo "----------------------------------------------------"
+echo "sudo apt update"
 sudo apt update
+echo "----------------------------------------------------"
+echo "sudo apt install software-properties-common apt-transport-https wget -y"
 sudo apt install software-properties-common apt-transport-https wget -y
+echo "----------------------------------------------------"
+echo "sudo wget -O -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -"
 sudo wget -O -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+echo "----------------------------------------------------"
+echo sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+echo "----------------------------------------------------"
+echo "sudo apt update"
 sudo apt update
+echo "----------------------------------------------------"
+echo "sudo apt install code -y"
 sudo apt install code -y
-code -V
+echo "----------------------------------------------------"
+echo "code -v"
+code -v
 echo " "
 echo "----------------------------------------------------"
 echo "Done Installing Visual Studion Code"
