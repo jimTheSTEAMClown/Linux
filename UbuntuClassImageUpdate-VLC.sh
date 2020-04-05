10533c5cad8ad1e6a2da2260232c661016c19cf8
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 12:45:42 -0700

    Update UbuntuClassImageUpdate-VLC.sh

--- UbuntuClassImageUpdate-VLC.sh
+++ UbuntuClassImageUpdate-VLC.sh
@@ -3,15 +3,15 @@
 # See: https://www.vlchelp.com/install-ubuntu-linux/ for some install tips.
 # You can take a look at https://www.vlchelp.com/vlc-media-player/ about it.
 echo "----------------------------------------------------"
-echo "installing VLC"
+echo "  [script VLC] Installing VLC"
 echo "----------------------------------------------------"
 echo " "
 echo "Installing VLC" 
 sudo apt-get install vlc -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo vlc -V
+vlc -V
 echo " "
 echo "----------------------------------------------------"
-echo "Done Installing VLC"
+echo "  [script VLC] Done Installing VLC"
 echo "----------------------------------------------------"
