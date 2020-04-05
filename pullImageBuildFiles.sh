c876142a5f75d90d7b6bcbceed11029524edcdc7
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-04-05 10:45:57 -0700

    Update pullImageBuildFiles.sh

--- pullImageBuildFiles.sh
+++ pullImageBuildFiles.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
-sudo wget https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate.sh
-sudo chmod 755 UbuntuClassImageUpdate.sh
-sudo cat UbuntuClassImageUpdate.sh
+cd ~
+echo " ----------------------------------------------------"
+echo "Create classImageBuild Directory where all these build files go"
+echo " ----------------------------------------------------"
+sudo mkdir classImageBuild
+echo " ----------------------------------------------------"
+echo "Copy UbunutClassImageUpdate.sh file"
+echo " ----------------------------------------------------"
+sudo wget -O ./classImageBuild/UbuntuClassImageUpdate.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate.sh
+sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate.sh
+sudo cat ./classImageBuild/UbuntuClassImageUpdate.sh
+echo " ----------------------------------------------------"
+echo "VLC build file"
+echo " ----------------------------------------------------"
+sudo wget -O ./classImageBuild/UbuntuClassImageUpdate-VLC.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/UbuntuClassImageUpdate-VLC.sh
+sudo chmod 755 ./classImageBuild/UbuntuClassImageUpdate-VLC.sh
+
+echo " ----------------------------------------------------"
+echo "Done copying all the classImageBuild files"
+echo " ----------------------------------------------------"
+cd classImageBuild
 # sudo bash UbuntuClassImageUpdate.sh
