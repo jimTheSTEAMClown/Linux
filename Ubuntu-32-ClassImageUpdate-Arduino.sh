6ba3f986dc6e85310ef45e81ab8161da94f7ef0d
Jim Burnham <jim.the.steam.clown@gmail.com>
2020-09-09 21:07:24 -0700

    Update Ubuntu-32-ClassImageUpdate-Arduino.sh

--- Ubuntu-32-ClassImageUpdate-Arduino.sh
+++ Ubuntu-32-ClassImageUpdate-Arduino.sh
@@ -6,8 +6,8 @@ echo "installing Arduino 32 BitIDE"
 echo "----------------------------------------------------"
 echo " "
 echo "----------------------------------------------------"
-echo "sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux32.tar.xz"
-sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux32.tar.xz
+echo "sudo wget -O arduino-1.8.13-linux32.tar.xz https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux32.tar.xz"
+sudo wget -O arduino-1.8.13-linux32.tar.xz https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux32.tar.xz 
 echo "----------------------------------------------------"
 echo "sudo tar xvf arduino-1.8.13-linux32.tar.xz -y"
 sudo tar xvf arduino-1.8.13-linux32.tar.xz -y
