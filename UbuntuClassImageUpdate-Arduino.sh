8f174555a9bec65047251e7c35e9b4e79eaace45
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-06-28 09:04:20 -0700

    Update UbuntuClassImageUpdate-Arduino.sh

--- UbuntuClassImageUpdate-Arduino.sh
+++ UbuntuClassImageUpdate-Arduino.sh
@@ -6,11 +6,13 @@ echo "installing Arduino 64 Bit IDE"
 echo "----------------------------------------------------"
 echo " "
 echo "----------------------------------------------------"
-echo "sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux64.tar.xz"
-sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.13-linux64.tar.xz
+# check on the arduino site to find the latest version. 
+
+echo "sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.15-linux64.tar.xz"
+sudo wget https://www.arduino.cc/download_handler.php?f=/arduino-1.8.15-linux64.tar.xz
 echo "----------------------------------------------------"
-echo "sudo tar xvf arduino-1.8.13-linux64.tar.xz -y"
-sudo tar xvf arduino-1.8.13-linux64.tar.xz -y
+echo "sudo tar xvf arduino-1.8.15-linux64.tar.xz -y"
+sudo tar xvf arduino-1.8.15-linux64.tar.xz -y
 echo "----------------------------------------------------"
 echo "ls"
 ls
