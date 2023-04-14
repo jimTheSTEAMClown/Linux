4a8c0ca9fc2fa05e7ea9025e3c64a4108b69e6da
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-04-14 16:55:24 -0700

    Create Ubuntu-22-04-2-CleanUpdate.sh

---
+++ Ubuntu-22-04-2-CleanUpdate.sh
@@ -0,0 +1,138 @@
+#! /bin/bash
+# ============================================================================
+# shell script to restore an Ubuntu Linux Class Image after a clean Ubuntu install
+# Source: STEAM Clown - www.steamclown.org 
+# GitHub: https://github.com/jimTheSTEAMClown/Linux
+# Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist 
+# This example code is licensed under the CC BY-NC-SA 4.0, GNU GPL and EUPL
+# https://creativecommons.org/licenses/by-nc-sa/4.0/
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# https://eupl.eu/
+# Program/Design Name:		Ubuntu-22-04-2-CleanUpdate.sh
+# Description:    shell script to restore an Ubuntu Linux Class Image after a clean Ubuntu installl 
+# Dependencies:   
+# Revision: 
+#  Revision 0.03 - Updated 04/14/2023 for SVCTE Mechatronics Class using Ubuntu 22.04.2
+#  - Added install for curl and git
+#  Revision 0.02 - Updated 04/04/2020 for SVCTE Mechatronics Class
+#  Revision 0.01 - Created 04/04/2020
+# Additional Comments: 
+# see https://www.answers.com/Q/How_do_you_make_a_yes_no_command_in_cmd to add more features
+# ============================================================================
+echo "----------------------------------------------------"
+echo "Ubuntu Class Image Update Script" 
+echo "----------------------------------------------------"
+echo " "
+pwd
+# ls -l
+ls
+echo " "
+echo "----------------------------------------------------"
+echo "Do you wish run the Ubuntu Class Image Update?"
+echo "----------------------------------------------------"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+            echo "----------------------------------------------------"
+            echo "Running Ubuntu Class Image Update" 
+            echo "----------------------------------------------------"
+            break;;
+        No ) 
+            echo "----------------------------------------------------"
+            echo "Exiting Without Update" 
+            echo "----------------------------------------------------"
+            exit;;
+    esac
+done
+# ============================================================================
+# update the software install to update with main universe restricted multiverse
+# echo "----------------------------------------------------"
+# echo "listing which software repositories are used - main universe restricted multiverse"
+# echo "----------------------------------------------------"
+# sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+# grep ^[^#] /etc/apt/sources.list
+# sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+echo " "
+
+echo "----------------------------------------------------"
+echo "Ready... I'm going to run "
+echo "  Going to run $ update with -y"
+echo "  Going to run $ upgrade with -y" 
+echo "----------------------------------------------------"
+echo "Do you wish to run UPDATE and UPGRADE?  Enter y/Y or n/N or any Key?"
+read -p "update and upgrade?: " yesInstall
+# elif statements
+if [ $yesInstall == "y" ] || [ $yesInstall == "Y" ]
+then
+echo "----------------------------------------------------"
+echo "getting home with cd ~"
+echo "----------------------------------------------------"
+echo " "
+cd ~
+pwd
+echo "----------------------------------------------------"
+echo "Running $ update"
+echo "----------------------------------------------------"
+echo " "
+sudo apt update
+echo " "
+echo "----------------------------------------------------"
+echo "Done running Update"
+echo "----------------------------------------------------"
+echo "----------------------------------------------------"
+echo "Running $ upgrade with -y"
+echo "----------------------------------------------------"
+echo " "
+sudo apt upgrade -y
+echo " "
+echo "----------------------------------------------------"
+echo "Done running Upgrade"
+echo "----------------------------------------------------"
+elif [ $yesInstall == "n" ] || [ $yesInstall == "N" ]
+then
+echo "Skipping this install"
+else
+echo "Skipping this install"
+fi
+
+echo "Now I'm going to install some standard apps that are not part"
+echo "of the standard Ubuntu build, but I find you will use them lots..."
+echo "I'm installing:"
+echo " - curl"
+echo " - git"
+echo " "
+echo "Do you wish to install these? Dude, just say 'Yes' Trust me."
+echo "Enter y/Y or n/N or any Key?"
+read -p "Install this list of apps?: " yesInstall
+# elif statements
+if [ $yesInstall == "y" ] || [ $yesInstall == "Y" ]
+then
+echo "----------------------------------------------------"
+echo "Installing curl"
+echo "Running $ sudo apt install curl"
+echo "----------------------------------------------------"
+echo " "
+sudo apt install curl
+echo " "
+echo "Installing git"
+echo "Running $ sudo apt install git"
+echo "----------------------------------------------------"
+echo " "
+sudo apt install git
+echo " "
+echo "----------------------------------------------------"
+echo "Done running App installs and updates"
+echo "----------------------------------------------------"
+elif [ $yesInstall == "n" ] || [ $yesInstall == "N" ]
+then
+echo "Skipping this install"
+else
+echo "Skipping this install"
+fi
+
+# sudo apt update
+# sudo apt upgrade -y
+echo "----------------------------------------------------"
+echo "Done running Ubuntu 22.04.2 Class Image Build"
+echo "----------------------------------------------------"
+echo " "
