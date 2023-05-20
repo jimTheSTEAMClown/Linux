da1d7361b090872900e59a577b98a3d65ef4be1c
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-05-19 17:09:52 -0700

    Update Ubuntu-22-04-2-CleanUpdate.sh

--- Ubuntu-22-04-2-CleanUpdate.sh
+++ Ubuntu-22-04-2-CleanUpdate.sh
@@ -152,6 +152,7 @@ cd tmp-download
 wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 cd ~
+which google-chrome
 
 echo " "
 echo "----------------------------------------------------"
