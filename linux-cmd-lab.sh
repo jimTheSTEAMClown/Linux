561b97b3255f03d285bccb768dc04c8fd12b6a64
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-03-10 12:25:41 -0700

    Update linux-cmd-lab.sh

--- linux-cmd-lab.sh
+++ linux-cmd-lab.sh
@@ -25,6 +25,7 @@
 cd ~
 mkdir linuxCommandLab
 # cd linuxCommandLab
-mkdir ./linuxCommandLab/labFiles_1
-sudo wget -O ./linuxCommandLab/labFiles_1/my_file_1.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_1.txt
-sudo wget -O ./linuxCommandLab/labFiles_1/my_file_2.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_2.txt
+mkdir ./linuxCommandLab/labTxtFiles
+sudo wget -O ./linuxCommandLab/labTxtFiles/my_txt_file_1.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_1.txt
+sudo wget -O ./linuxCommandLab/labTxtFiles/my_txt_file_2.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_2.txt
+sudo wget -O ./linuxCommandLab/labTxtFiles/my_txt_file_3.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_3.txt
