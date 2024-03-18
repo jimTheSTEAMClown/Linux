a8818554e715c46644803408171210804a11fb12
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-03-18 08:43:52 -0700

    Update linux-cmd-lab.sh

--- linux-cmd-lab.sh
+++ linux-cmd-lab.sh
@@ -18,13 +18,28 @@
 #  Revision 0.01 - Created 03/10/2024
 # Additional Comments:
 # ============================================================================
+echo " "
+echo " ______       __                __   __           "
+echo " | ___ \     / _|               \ \ / /           "
+echo " | |_/ / ___| |_ ___  _ __ ___   \ V /___  _   _  "
+echo " | ___ \/ _ \  _/ _ \| '__/ _ \   \ // _ \| | | | "
+echo " | |_/ /  __/ || (_) | | |  __/   | | (_) | |_| | "
+echo " \____/ \___|_| \___/|_|  \___|   \_/\___/ \__,_| "
+echo " "                                                 
+echo " ______                    _                 _    "
+echo " |  _  \                  | |               | |   "
+echo " | | | |_____      ___ __ | | ___   __ _  __| |   "
+echo " | | | / _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |   "
+echo " | |/ / (_) \ V  V /| | | | | (_) | (_| | (_| |   "
+echo " |___/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|   "
+echo " "                                                                                                  
 # copy to your /home/<USER> directory
 # sudo wget -O ./linux-cmd-lab.sh https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux-cmd-lab.sh
 # sudo chmod 755 ./linux-cmd-lab.sh
 # sudo cat ./linux-cmd-lab.sh
+# ============================================================================
 cd ~
 mkdir linuxCommandLab
-# cd linuxCommandLab
 mkdir ./linuxCommandLab/labTxtFiles
 sudo wget -O ./linuxCommandLab/labTxtFiles/my_txt_file_1.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_1.txt
 sudo wget -O ./linuxCommandLab/labTxtFiles/my_txt_file_2.txt https://raw.githubusercontent.com/jimTheSTEAMClown/Linux/master/linux_cmd_lab_my_file_2.txt
